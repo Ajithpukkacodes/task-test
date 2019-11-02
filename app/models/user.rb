b35532cdc31ab1812cdbd4b01bc8b67f4953f2a0
Ajithpukka <ajith.kumar@pukkacodes.com>
2019-11-02 20:16:13 +0530

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # reverse_geocoded_by :latitude, :longitude
+end
---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+require 'faker'
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+
+# 10.times do |i|
+#   User.create!(
+#     email: Faker::Internet.email,
+#     first_name: Faker::Name.first_name,
+#     password: 'password',
+#     second_name: Faker::Name.last_name,
+#     latitude: Faker::Address.latitude,
+#     longitude: Faker::Address.longitude,
+#   )
+
+#   puts "*** created user #{i}"
+# end
